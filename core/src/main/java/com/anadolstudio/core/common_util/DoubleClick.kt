591fa29419e1ff88b16a8b9113c85acb5e091041
Anadol <74777850+AnadolStudio@users.noreply.github.com>
2022-09-11 01:37:54 +0300

    fix DoubleClick

--- core/src/main/java/com/anadolstudio/core/common_util/DoubleClick.kt
+++ core/src/main/java/com/anadolstudio/core/common_util/DoubleClick.kt
@@ -8,13 +8,14 @@ open class DoubleClick(private val delay: Long = DEFAULT_DELAY) {
 
     fun onDoubleClickAction(onSimpleClick: (() -> Unit)? = null, onDoubleClick: () -> Unit) {
         val currentTime = System.currentTimeMillis()
+        val wasDoubleClick = lastClickTime + delay > currentTime
 
-        when (lastClickTime + delay > currentTime) {
+        when (wasDoubleClick) {
             true -> onDoubleClick.invoke()
             false -> onSimpleClick?.invoke()
         }
 
-        lastClickTime = currentTime
+        lastClickTime = if (wasDoubleClick) 0L else currentTime
     }
 }
 
