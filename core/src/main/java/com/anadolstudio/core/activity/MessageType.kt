61e3e9e18a02b1011b78f20f2a1ace499d6459fd
Anadol <74777850+AnadolStudio@users.noreply.github.com>
2022-09-16 01:29:01 +0300

    update BaseActivity.kt

--- core/src/main/java/com/anadolstudio/core/activity/MessageType.kt
+++
@@ -1,8 +0,0 @@
-package com.anadolstudio.core.activity
-
-sealed class MessageType(val text: String) {
-
-    class Toast(text: String) : MessageType(text)
-
-    class Snack(text: String) : MessageType(text)
-}
--- core/src/main/java/com/anadolstudio/core/viewmodel/BaseViewModel.kt
+++ core/src/main/java/com/anadolstudio/core/viewmodel/BaseViewModel.kt
@@ -1,10 +1,16 @@
 package com.anadolstudio.core.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.anadolstudio.core.livedata.SingleEvent
+import com.anadolstudio.core.livedata.SingleLiveEvent
+import com.anadolstudio.core.livedata.toImmutable
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 
 abstract class BaseViewModel : ViewModel() {
+
+    protected val _singleEvent = SingleLiveEvent<SingleEvent>()
+    val event = _singleEvent.toImmutable()
 
     private val compositeDisposable by lazy { CompositeDisposable() }
 
