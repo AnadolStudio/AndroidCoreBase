b73c970f7e346359313df2100066827723fcc2c0
Anadol <74777850+AnadolStudio@users.noreply.github.com>
2023-01-05 02:24:26 +0300

    create Toolbar

--- core/src/main/java/com/anadolstudio/core/common_extention/ContextExt.kt
+++ core/src/main/java/com/anadolstudio/core/common_extention/ContextExt.kt
@@ -2,8 +2,11 @@ package com.anadolstudio.core.common_extention
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.provider.Settings
+import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
 
 fun Context.startAppSettingsActivity() {
     val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
@@ -12,3 +15,5 @@ fun Context.startAppSettingsActivity() {
 
     startActivity(intent)
 }
+
+fun Context.getCompatDrawable(@DrawableRes id: Int): Drawable? = ContextCompat.getDrawable(this, id)
