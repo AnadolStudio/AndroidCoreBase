306d7aca7283e1fe95049da204d8e43f51248149
Anadol <74777850+AnadolStudio@users.noreply.github.com>
2023-01-08 22:55:06 +0300

    add FragmentStateUtil

---
+++ core/src/main/java/com/anadolstudio/core/fragment/state_util/State.kt
@@ -0,0 +1,11 @@
+package com.anadolstudio.core.fragment.state_util
+
+import android.view.View
+import com.anadolstudio.core.fragment.state_util.strategy.ShowOnEnterGoneOnExitStrategy
+import com.anadolstudio.core.fragment.state_util.strategy.base.StateChangeStrategy
+
+data class State<T>(
+        val name: T,
+        val viewsGroup: List<View>,
+        val strategy: StateChangeStrategy<T> = ShowOnEnterGoneOnExitStrategy()
+) where T : Enum<T>
