f2a0b2f904798605da30d06b6ce6f3b91e43c249
Bogdan Terehov <bterehov@licard.com>
2023-08-29 04:11:01 +0300

    remove ContentableState.kt

--- core/src/main/java/com/anadolstudio/core/presentation/Contentable.kt
+++ core/src/main/java/com/anadolstudio/core/presentation/Contentable.kt
@@ -3,26 +3,10 @@ package com.anadolstudio.core.presentation
 import com.anadolstudio.core.presentation.fragment.state_util.ViewStateDelegate
 import com.anadolstudio.core.viewmodel.BaseController
 
-interface Contentable<ViewState : ContentableState, Controller : BaseController> {
+interface Contentable<ViewState : Any, Controller : BaseController> {
 
     val viewStateDelegate: ViewStateDelegate
 
-    fun render(state: ViewState, controller: Controller) = when {
-        state.isShimmers -> showShimmers()
-        state.isContent -> showContent(state)
-        state.isError -> state.getError?.let { showError(it, controller) }
-        else -> Unit
-    }
+    fun render(state: ViewState, controller: Controller)
 
-    fun showContent(content: ViewState) {
-        viewStateDelegate.showContent()
-
-        showFullScreenLoading(content.isFullScreenLoading)
-        if (content.isEmpty) showEmpty()
-    }
-
-    fun showShimmers() = viewStateDelegate.showLoading()
-    fun showFullScreenLoading(isFullScreenLoading: Boolean) = Unit
-    fun showEmpty() = viewStateDelegate.showStub()
-    fun showError(error: Throwable, controller: Controller) = viewStateDelegate.showError()
 }
