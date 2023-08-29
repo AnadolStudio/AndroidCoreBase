8566c9b7e0294745f64daca32f706428a17e0001
Bogdan Terehov <bterehov@licard.com>
2023-08-29 03:55:35 +0300

    remove SpaceItemDecoration.kt, and move BaseSpaceItemDecoration.kt

---
+++ core/src/main/java/com/anadolstudio/core/recycler/BaseSpaceItemDecoration.kt
@@ -0,0 +1,44 @@
+package com.anadolstudio.core.recycler
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.anadolstudio.core.common_util.dpToPx
+
+open class BaseSpaceItemDecoration(
+        private val padding: Rect = Rect(MEDIUM_SPACE, MEDIUM_SPACE, MEDIUM_SPACE, MEDIUM_SPACE),
+) : RecyclerView.ItemDecoration() {
+
+    constructor(space: Int = MEDIUM_SPACE) : this(Rect(space, space, space, space))
+
+    companion object {
+        val SMALL_SPACE = 4.dpToPx()
+        val MEDIUM_SPACE = 8.dpToPx()
+        val EXTRA_MEDIUM_SPACE = 12.dpToPx()
+        val MAIN_SPACE = 16.dpToPx()
+    }
+
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+        outRect.set(padding)
+    }
+
+    class Horizontal(space: Int = MEDIUM_SPACE) : BaseSpaceItemDecoration(Rect(space, 0, space, 0))
+
+    class Vertical(space: Int = MEDIUM_SPACE) : BaseSpaceItemDecoration(Rect(0, space, 0, space))
+
+    class All(space: Int = MEDIUM_SPACE) : BaseSpaceItemDecoration(space)
+
+    class CornerItems(
+            private val columnCount: Int = 1,
+            private val space: Int = MEDIUM_SPACE
+    ) : BaseSpaceItemDecoration(space) {
+        override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+            val index = parent.getChildAdapterPosition(view)
+
+            when {
+                index % columnCount == 0 -> outRect.set(space, 0, 0, 0)
+                index % columnCount == columnCount - 1 -> outRect.set(0, 0, space, 0)
+            }
+        }
+    }
+}
