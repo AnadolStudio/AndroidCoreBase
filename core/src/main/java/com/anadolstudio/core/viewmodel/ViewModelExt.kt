0436f722727088129f2a5726b53269b864140795
Bogdan Terehov <bogdan.terehov>
2023-09-02 19:50:00 +0300

    update gradle to 8.3

--- core/src/main/java/com/anadolstudio/core/viewmodel/ViewModelExt.kt
+++ core/src/main/java/com/anadolstudio/core/viewmodel/ViewModelExt.kt
@@ -4,10 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import com.anadolstudio.core.viewmodel.livedata.SingleLiveEvent
 
 inline fun <reified T : ViewModel> FragmentActivity.obtainViewModel(
@@ -20,7 +18,7 @@ inline fun <reified T : ViewModel> FragmentActivity.obtainViewModel(
         viewModelFactory: ViewModelProvider.Factory,
         body: T.() -> Unit
 ): T {
-    val vm = ViewModelProviders.of(this, viewModelFactory)[T::class.java]
+    val vm = ViewModelProvider(this, viewModelFactory)[T::class.java]
     vm.body()
     return vm
 }
@@ -44,26 +42,26 @@ fun <T : Any, L : LiveData<T>> Fragment.observe(
         liveData: L,
         block: (T) -> Unit
 ) {
-    liveData.observe(this.viewLifecycleOwner, Observer<T> { it?.let { block.invoke(it) } })
+    liveData.observe(this.viewLifecycleOwner) { it.let { block.invoke(it) } }
 }
 
 fun <L : SingleLiveEvent<Unit>> Fragment.observe(
         singleLiveEvent: L,
         block: () -> Unit
 ) {
-    singleLiveEvent.observe(this.viewLifecycleOwner, Observer { block.invoke() })
+    singleLiveEvent.observe(this.viewLifecycleOwner) { block.invoke() }
 }
 
 fun <T : Any, L : LiveData<T>> AppCompatActivity.observe(
         liveData: L,
         block: (T) -> Unit
 ) {
-    liveData.observe(this, Observer<T> { it?.let { block.invoke(it) } })
+    liveData.observe(this) { it.let { block.invoke(it) } }
 }
 
 fun <L : SingleLiveEvent<Unit>> AppCompatActivity.observe(
         singleLiveEvent: L,
         block: () -> Unit
 ) {
-    singleLiveEvent.observe(this, Observer { block.invoke() })
+    singleLiveEvent.observe(this) { block.invoke() }
 }
