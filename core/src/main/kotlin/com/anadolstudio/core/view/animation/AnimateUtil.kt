faff34bf280d4ba60286e4b0e731b115345d6c67
Bogdan Terehov <bogdan.terehov>
2023-09-03 21:45:37 +0300

    feature: update AnimateUtil

--- core/src/main/kotlin/com/anadolstudio/core/view/animation/AnimateUtil.kt
+++ core/src/main/kotlin/com/anadolstudio/core/view/animation/AnimateUtil.kt
@@ -19,10 +19,13 @@ import com.anadolstudio.core.util.common_extention.makeGone
 import com.anadolstudio.core.util.common_extention.makeVisible
 
 object AnimateUtil {
-    const val DURATION_SHORT: Long = 200
+    const val DURATION_SHORT: Long = 100
+    const val DURATION_EXTRA_SHORT: Long = 200
     const val DURATION_NORMAL: Long = 300
     const val DURATION_LONG: Long = 400
     const val DURATION_EXTRA_LONG: Long = 800
+    const val START = -1
+    const val END = 1
 
     const val REDUCE_SCALE_CLICK = 0.9F
     const val INCREASE_SCALE_CLICK = 1.1F
@@ -57,7 +60,7 @@ object AnimateUtil {
 
     fun <T : View> fadOutAnimation(
             view: T,
-            duration: Long = DURATION_SHORT,
+            duration: Long = DURATION_EXTRA_SHORT,
             visibility: Int = INVISIBLE,
             completion: ((T) -> Unit)? = null
     ) {
@@ -74,7 +77,7 @@ object AnimateUtil {
 
     fun <T : View> fadInAnimation(
             view: T,
-            duration: Long = DURATION_SHORT, completion: ((T) -> Unit)? = null
+            duration: Long = DURATION_EXTRA_SHORT, completion: ((T) -> Unit)? = null
     ) {
         with(view) {
             alpha = 0f
@@ -86,8 +89,8 @@ object AnimateUtil {
         }
     }
 
-    fun View.showTranslationTopOut(duration: Long = DURATION_NORMAL) {
-        if (!isVisible) return
+    fun View.showTranslationTopOut(duration: Long = DURATION_NORMAL, safeVisible: Boolean = true) {
+        if (safeVisible && !isVisible) return
 
         visibility = GONE
         translationY = 0F
@@ -98,8 +101,8 @@ object AnimateUtil {
                 .setListener(getSimpleEndListener(this))
     }
 
-    fun View.showTranslationTopIn(duration: Long = DURATION_NORMAL) {
-        if (isVisible) return
+    fun View.showTranslationTopIn(duration: Long = DURATION_NORMAL, safeVisible: Boolean = true) {
+        if (safeVisible && isVisible) return
 
         translationY = -height.toFloat()
         animate()
@@ -107,6 +110,29 @@ object AnimateUtil {
                 .setDuration(duration)
                 .setInterpolator(DecelerateInterpolator())
                 .setListener(getSimpleStartListener(this))
+    }
+
+    fun View.showTranslationStartOutEndIn(duration: Long = DURATION_NORMAL, onEndAction: (() -> Unit)? = null) =
+            outEdgeInEge(END, duration, onEndAction)
+
+    fun View.showTranslationEndOutStartIn(duration: Long = DURATION_NORMAL, onEndAction: (() -> Unit)? = null) =
+            outEdgeInEge(START, duration, onEndAction)
+
+    private fun View.outEdgeInEge(direction: Int, duration: Long, onEndAction: (() -> Unit)?) {
+        animate()
+                .translationX(direction * width.toFloat())
+                .setDuration(duration)
+                .setInterpolator(DecelerateInterpolator())
+                .withEndAction {
+                    translationX = -1 * direction * width.toFloat()
+                    animate()
+                            .translationX(0F)
+                            .setDuration(duration)
+                            .setInterpolator(DecelerateInterpolator())
+                            .withEndAction { onEndAction?.invoke() }
+                            .start()
+                }
+                .start()
     }
 
     fun ViewGroup.animSlideOut(@Slide.GravityFlag slideEdge: Int, duration: Long = DURATION_NORMAL) {
@@ -129,13 +155,29 @@ object AnimateUtil {
         makeVisible()
     }
 
-    fun ViewGroup.animSlideTopOut(duration: Long = DURATION_NORMAL) = animSlideOut(Gravity.TOP, duration)
+    fun ViewGroup.animSlideTopOut(duration: Long = DURATION_NORMAL) =
+            animSlideOut(Gravity.TOP, duration)
 
-    fun ViewGroup.animSlideTopIn(duration: Long = DURATION_NORMAL) = animSlideIn(Gravity.TOP, duration)
+    fun ViewGroup.animSlideTopIn(duration: Long = DURATION_NORMAL) =
+            animSlideIn(Gravity.TOP, duration)
 
-    fun ViewGroup.animSlideBottomOut(duration: Long = DURATION_NORMAL) = animSlideOut(Gravity.BOTTOM, duration)
+    fun ViewGroup.animSlideBottomOut(duration: Long = DURATION_NORMAL) =
+            animSlideOut(Gravity.BOTTOM, duration)
 
-    fun ViewGroup.animSlideBottomIn(duration: Long = DURATION_NORMAL) = animSlideIn(Gravity.BOTTOM, duration)
+    fun ViewGroup.animSlideBottomIn(duration: Long = DURATION_NORMAL) =
+            animSlideIn(Gravity.BOTTOM, duration)
+
+    fun ViewGroup.animSlideStartOut(duration: Long = DURATION_NORMAL) =
+            animSlideOut(Gravity.START, duration)
+
+    fun ViewGroup.animSlideStartIn(duration: Long = DURATION_NORMAL) =
+            animSlideIn(Gravity.START, duration)
+
+    fun ViewGroup.animSlideEndIn(duration: Long = DURATION_NORMAL) =
+            animSlideIn(Gravity.END, duration)
+
+    fun ViewGroup.animSlideEndOut(duration: Long = DURATION_NORMAL) =
+            animSlideOut(Gravity.END, duration)
 
     fun showAnimY(view: View, start: Float, end: Float, listener: AnimatorListenerAdapter) {
         view.visibility = INVISIBLE
@@ -203,7 +245,7 @@ object AnimateUtil {
     private fun View.scaleAnimation(scale: Float, action: (() -> Unit)? = null) = animate()
             .scaleX(scale)
             .scaleY(scale)
-            .setDuration(DURATION_SHORT)
+            .setDuration(DURATION_EXTRA_SHORT)
             .withStartAction { action?.invoke() }
             .setInterpolator(DecelerateInterpolator())
             .start()
