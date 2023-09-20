abacb6702ad4bf039a76a0406471743c1b987bb6
bogdan <terehov.bogdan01@gmail.com>
2023-09-20 22:21:40 +0300

    fix ScaleGesture: поправил лишние отрабатывания жеста

--- core/src/main/kotlin/com/anadolstudio/core/view/gesture/ScaleGesture.kt
+++ core/src/main/kotlin/com/anadolstudio/core/view/gesture/ScaleGesture.kt
@@ -2,6 +2,7 @@ package com.anadolstudio.core.view.gesture
 
 import android.view.ScaleGestureDetector
 import com.anadolstudio.core.util.common_extention.onTrue
+import kotlin.math.abs
 
 class ScaleGesture(
         private val onIncrease: () -> Unit,
@@ -10,6 +11,7 @@ class ScaleGesture(
 
     private companion object {
         const val DELAY = 750L
+        const val SPAN_SLOP = 150
     }
 
     private var previousHandleTime: Long = 0
@@ -17,13 +19,20 @@ class ScaleGesture(
     override fun onScale(detector: ScaleGestureDetector): Boolean {
 
         return when {
-            detector.scaleFactor < 1F && isNotPause() -> true.also { onIncrease.invoke() }
-            detector.scaleFactor > 1F && isNotPause() -> true.also { onDecrease.invoke() }
+            detector.scaleFactor < 1F && detector.gestureTolerance() -> true.also { onIncrease.invoke() }
+            detector.scaleFactor > 1F && detector.gestureTolerance() -> true.also { onDecrease.invoke() }
             else -> false
         }.onTrue {
             previousHandleTime = System.currentTimeMillis()
         }
     }
 
+    private fun ScaleGestureDetector.gestureTolerance(): Boolean {
+        val spanDelta = abs(currentSpan - previousSpan)
+
+        return spanDelta > SPAN_SLOP && isNotPause()
+    }
+
     private fun isNotPause(): Boolean = System.currentTimeMillis() - previousHandleTime > DELAY
+
 }
