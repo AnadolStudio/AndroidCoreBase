2e6e4fbc28a6d95fc09f349add4a7192f3b4a500
Bogdan Terehov <bogdan.terehov>
2023-11-21 11:58:41 +0300

    feature: усовершенствовал Renderable

--- core/src/main/kotlin/com/anadolstudio/core/presentation/Renderable.kt
+++ core/src/main/kotlin/com/anadolstudio/core/presentation/Renderable.kt
@@ -2,27 +2,30 @@ package com.anadolstudio.core.presentation
 
 interface Renderable {
 
-    val stateMap: MutableMap<String, Int>
+    val stateMap: MutableMap<String, Any?>
 
     fun <State : Any> State.render(key: String, render: State.() -> Unit) {
-        if (stateMap[key] == hashCode()) return
+        if (!hasChanges(stateMap[key], this)) return
 
-        stateMap[key] = hashCode()
+        stateMap[key] = this
 
         render.invoke(this)
     }
 
     fun <State : Any> State.render(primaryKey: String, vararg dependentStates: Pair<String, Any?>, render: State.() -> Unit) {
-        val dependentStateMap = dependentStates.toMap()
-                .mapValues { it.hashCode() }
-                .toMutableMap()
-        dependentStateMap[primaryKey] = this.hashCode()
+        val dependentStateMap = dependentStates.toMap().toMutableMap()
+        dependentStateMap[primaryKey] = this
 
-        if (dependentStateMap.all { (key, hashCode) -> stateMap[key] == hashCode }) return
+        if (dependentStateMap.all { (key, item) -> !hasChanges(stateMap[key], item) }) return
 
-        dependentStateMap.forEach { (key, hashCode) -> stateMap[key] = hashCode }
+        dependentStateMap.forEach { (key, item) -> stateMap[key] = item }
 
         render.invoke(this)
     }
 
+    private fun areSame(old: Any?, new: Any?): Boolean = old.hashCode() == new.hashCode()
+
+    private fun equals(old: Any?, new: Any?): Boolean = old == new
+
+    private fun hasChanges(old: Any?, new: Any?) = !areSame(old, new) || !equals(old, new)
 }
