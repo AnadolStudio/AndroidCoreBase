de63c1dd0a0f8d4895a27027e0d5e1b82a098fb5
Bogdan Terehov <bogdan.terehov>
2023-11-22 09:57:04 +0300

    feature BaseToolbar: добавил description

--- core/src/main/kotlin/com/anadolstudio/core/util/common_extention/ViewExt.kt
+++ core/src/main/kotlin/com/anadolstudio/core/util/common_extention/ViewExt.kt
@@ -1,6 +1,8 @@
 package com.anadolstudio.core.util.common_extention
 
 import android.view.View
+import android.widget.TextView
+import androidx.core.view.isVisible
 
 fun View.makeGone() {
     this.visibility = View.GONE
@@ -26,4 +28,22 @@ fun View.setSmartPadding(
             end ?: paddingEnd,
             bottom ?: paddingBottom,
     )
+}
+
+fun View.enable() {
+    isEnabled = true
+}
+
+fun View.disable() {
+    isEnabled = false
+}
+
+fun TextView.setTextOrMakeGone(text: CharSequence?) {
+    this.text = text
+    isVisible = text != null
+}
+
+fun TextView.setTextOrMakeGoneIfBlank(text: CharSequence?) {
+    this.text = text
+    isVisible = !text.isNullOrBlank()
 }
