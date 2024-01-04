8d66ec7b1c104aff804ef428d95b0e13b124afbb
Bogdan Terehov <bogdan.terehov>
2024-01-04 11:11:37 +0300

    feature: Добавил assistedViewModel factory

--- core/src/main/kotlin/com/anadolstudio/core/viewmodel/ViewModelExt.kt
+++ core/src/main/kotlin/com/anadolstudio/core/viewmodel/ViewModelExt.kt
@@ -1,11 +1,16 @@
 package com.anadolstudio.core.viewmodel
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.AbstractSavedStateViewModelFactory
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.savedstate.SavedStateRegistryOwner
 import com.anadolstudio.core.viewmodel.livedata.SingleLiveEvent
 
 inline fun <reified T : ViewModel> FragmentActivity.obtainViewModel(
@@ -64,4 +69,25 @@ fun <L : SingleLiveEvent<Unit>> AppCompatActivity.observe(
         block: () -> Unit
 ) {
     singleLiveEvent.observe(this) { block.invoke() }
+}
+
+inline fun <reified VM : ViewModel> Fragment.assistedViewModel(
+        crossinline creator: (SavedStateHandle) -> VM,
+): Lazy<VM> = viewModels { createAbstractSavedStateViewModelFactory(arguments, creator) }
+
+inline fun <reified T : ViewModel> SavedStateRegistryOwner.createAbstractSavedStateViewModelFactory(
+        arguments: Bundle? = Bundle(),
+        crossinline creator: (SavedStateHandle) -> T,
+): ViewModelProvider.Factory {
+    return object : AbstractSavedStateViewModelFactory(
+            owner = this@createAbstractSavedStateViewModelFactory,
+            defaultArgs = arguments,
+    ) {
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel> create(
+                key: String,
+                modelClass: Class<T>,
+                handle: SavedStateHandle,
+        ): T = creator(handle) as T
+    }
 }
