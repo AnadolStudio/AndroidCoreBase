a3054b1c8afd894a9df4fce302b7769865c78b3a
Bogdan Terehov <bogdan.terehov>
2024-01-08 23:31:03 +0300

    feature: add Parcelize to Time

--- core/src/main/kotlin/com/anadolstudio/core/util/data_time/DataTimeExt.kt
+++ core/src/main/kotlin/com/anadolstudio/core/util/data_time/DataTimeExt.kt
@@ -1,6 +1,9 @@
 package com.anadolstudio.core.util.data_time
 
+import android.os.Parcelable
 import com.anadolstudio.core.util.common_extention.tryOrNull
+import kotlinx.parcelize.IgnoredOnParcel
+import kotlinx.parcelize.Parcelize
 import org.joda.time.DateTime
 
 val Long.hours: Int get() = (this / 3600000).toInt()
@@ -9,22 +12,28 @@ val Long.seconds: Int get() = ((this - hours * 3600000 - minutes * 60000) / 1000
 
 fun String.safeParseDateTime(): DateTime? = tryOrNull { DateTime.parse(this) }
 
+@Parcelize
 data class Time(
-    val hours: Int,
-    val minutes: Int,
-    val seconds: Int,
-) {
+        val hours: Int,
+        val minutes: Int,
+        val seconds: Int,
+) : Parcelable {
     constructor(millis: Long) : this(
-        hours = millis.hours,
-        minutes = millis.minutes,
-        seconds = millis.seconds,
+            hours = millis.hours,
+            minutes = millis.minutes,
+            seconds = millis.seconds,
     )
 
+    @IgnoredOnParcel
     val hoursString: String = hours.toTimeNumber()
+    @IgnoredOnParcel
     val minutesString: String = minutes.toTimeNumber()
+    @IgnoredOnParcel
     val secondsString: String = seconds.toTimeNumber()
 
+    @IgnoredOnParcel
     val totalMinutes: Int = hours * 60 + minutes + seconds / 60
+
 }
 
 private fun Int.toTimeNumber(): String = if (this < 10) "0$this" else "$this"
