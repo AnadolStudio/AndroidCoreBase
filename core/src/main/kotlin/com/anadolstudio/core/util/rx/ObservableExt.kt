58246fa7a0ec784d7dfc2d2b99d2e200c1508832
Bogdan Terehov <bogdan.terehov>
2024-01-10 14:15:38 +0300

    feature: добавил @CheckReturnValue в ObservableExt.kt

--- core/src/main/kotlin/com/anadolstudio/core/util/rx/ObservableExt.kt
+++ core/src/main/kotlin/com/anadolstudio/core/util/rx/ObservableExt.kt
@@ -5,11 +5,13 @@ import io.reactivex.Flowable
 import io.reactivex.Maybe
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.annotations.CheckReturnValue
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 
 fun <T> Observable<T>.schedulersIoToMain(): Observable<T> = subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
 
+@CheckReturnValue
 fun <T> Observable<T>.smartSubscribe(
         isSchedulersIoToMain: Boolean = true,
         onSuccess: ((T) -> Unit)? = null,
@@ -36,23 +38,29 @@ fun <T> Flowable<T>.schedulersIoToMain(): Flowable<T> = subscribeOn(Schedulers.i
 
 fun Completable.schedulersIoToMain(): Completable = subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
 
+@CheckReturnValue
 fun Completable.smartSubscribe(
+        isSchedulersIoToMain: Boolean = true,
         onSubscribe: (() -> Unit)? = null,
         onComplete: (() -> Unit)? = null,
         onError: ((Throwable) -> Unit)? = null,
         onFinally: (() -> Unit)? = null
-): Disposable = this
-        .doOnSubscribe { onSubscribe?.invoke() }
-        .subscribe(
-        {
-            onComplete?.invoke()
-            onFinally?.invoke()
-        },
-        { error ->
-            onError?.invoke(error)
-            onFinally?.invoke()
-        }
-)
+): Disposable {
+    val completable = if (isSchedulersIoToMain) this.schedulersIoToMain() else this
+
+    return completable
+            .doOnSubscribe { onSubscribe?.invoke() }
+            .subscribe(
+                    {
+                        onComplete?.invoke()
+                        onFinally?.invoke()
+                    },
+                    { error ->
+                        onError?.invoke(error)
+                        onFinally?.invoke()
+                    }
+            )
+}
 
 fun <T> Maybe<T>.schedulersIoToMain(): Maybe<T> = subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
 
