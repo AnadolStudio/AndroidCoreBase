713b7158cb3c38b738b9eb84abf64b4b81321167
Bogdan Terehov <bogdan.terehov>
2024-07-13 00:48:46 +0300

    feature: add ViewExt.kt

--- utils/src/main/kotlin/com/anadolstudio/utils/util/extentions/ViewExt.kt
+++ utils/src/main/kotlin/com/anadolstudio/utils/util/extentions/ViewExt.kt
@@ -88,6 +88,9 @@ fun View.setDimensMargins(start: Int? = null, top: Int? = null, end: Int? = null
 
 fun View.getMarginLayoutParams(): ViewGroup.MarginLayoutParams? = layoutParams as? ViewGroup.MarginLayoutParams
 
+val View.centerX:Int get() = width / 2
+val View.centerY:Int get() = height / 2
+
 fun ImageView.setImageFromUrl(
     url: String?,
     @DrawableRes errorId: Int? = null,
