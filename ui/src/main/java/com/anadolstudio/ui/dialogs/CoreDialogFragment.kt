5c8651bb2cfa3014a75b199115fcbaf58460248f
Bogdan Terehov <bogdan.terehov>
2024-08-14 16:44:37 +0300

    feature: refactor dialog

--- ui/src/main/java/com/anadolstudio/ui/dialogs/CoreDialogFragment.kt
+++ ui/src/main/java/com/anadolstudio/ui/dialogs/CoreDialogFragment.kt
@@ -5,27 +5,55 @@ import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatDialogFragment
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.fragment.app.FragmentManager
 
 abstract class CoreDialogFragment(@LayoutRes private val layoutId: Int) : AppCompatDialogFragment(layoutId) {
 
-    abstract fun getDialogTag(): String
+    protected open var isStatusBarByNightMode: Boolean = true
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setTransparentStatusBar()
-    }
+    abstract fun getDialogTag(): String
 
     override fun onStart() {
         super.onStart()
         with(dialog) {
             // Для корректной отрисовки диалога
             this?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
-            this?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            this?.window?.let { setupBackgroundColor(it) }
+        }
+        updateStatusBarIcons()
+    }
+
+    protected open fun setupBackgroundColor(window: Window) {
+        window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+    }
+
+    private fun updateStatusBarIcons() {
+        if (isStatusBarByNightMode) {
+            setDarkStatusBarIcon()
+        } else {
+            setLightStatusBarIcon()
         }
     }
+
+    protected fun setDarkStatusBarIcon() = setStatusBarIconColor(isDark = true)
+
+    protected fun setLightStatusBarIcon() = setStatusBarIconColor(isDark = false)
+
+    private fun setStatusBarIconColor(isDark: Boolean) {
+        val window = getWindow() ?: return
+        val decorView = getDecorView() ?: return
+
+        val insetsController = WindowInsetsControllerCompat(window, decorView)
+        insetsController.isAppearanceLightStatusBars = isDark
+    }
+
+    protected fun getWindow(): Window? = requireDialog().window
+
+    protected fun getDecorView(): View? = getWindow()?.decorView
 
     fun show(fragmentManager: FragmentManager) {
         val fragmentTransaction = fragmentManager.beginTransaction()
@@ -34,13 +62,6 @@ abstract class CoreDialogFragment(@LayoutRes private val layoutId: Int) : AppCom
         fragmentManager.findFragmentByTag(getDialogTag())?.let(fragmentTransaction::remove)
 
         this.show(fragmentTransaction, getDialogTag())
-    }
-
-    private fun setTransparentStatusBar() {
-        dialog?.window?.apply {
-            decorView.systemUiVisibility = decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-            statusBarColor = Color.TRANSPARENT
-        }
     }
 
 }
