e1cbedec6e041b7cea88efb25899b808e3f5bf06
Bogdan Terehov <bogdan.terehov>
2024-08-15 19:20:53 +0300

    feature: "From" to NavigationEvent.kt

--- ui/src/main/java/com/anadolstudio/ui/navigation/NavigationEvent.kt
+++ ui/src/main/java/com/anadolstudio/ui/navigation/NavigationEvent.kt
@@ -3,6 +3,11 @@ package com.anadolstudio.ui.navigation
 sealed class NavigationEvent<Data>
 
 open class Back<Data>(val data: Data? = null) : NavigationEvent<Data>()
+
 open class BackTo<Data>(val data: Data) : NavigationEvent<Data>()
+
+open class From<Data>(val data: Data, val fromId: Int? = null) : NavigationEvent<Data>()
+
 open class Replace<Data>(val data: Data) : NavigationEvent<Data>()
+
 open class Add<Data>(val data: Data) : NavigationEvent<Data>()
